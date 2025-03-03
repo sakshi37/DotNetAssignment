994fc5afef00e32718254a04a922a9d698effffd
sakshi37 <sakshibahirat14@gmail.com>
2025-03-03 16:23:52 +0530

    commit

--- Day6_Assignment/Task2/Program.cs
+++ Day6_Assignment/Task2/Program.cs
@@ -8,13 +8,13 @@ namespace Task2
         {
             Hashtable student = new Hashtable();
 
-            Console.WriteLine("Enrolled for the cours which you want to\n How many courses do you wnat to enrolled");
+            Console.WriteLine("Enrolled for the Workshop which you want to\n How many Workshop do you want to enrolled");
 
             int num = Convert.ToInt32(Console.ReadLine());
 
             for (int i=1; i<=num;i++)
             {
-                Console.WriteLine("enter Student id for your course");
+                Console.WriteLine("enter Student id for workshop");
                 int key =Convert.ToInt32( Console.ReadLine());
 
                 if (student.Contains(key))
@@ -24,7 +24,7 @@ namespace Task2
                 }
                 else
                 {
-                    Console.WriteLine("Enter the name of your Course");
+                    Console.WriteLine("Enter the name of your workshop");
                     String course = Console.ReadLine();
 
                     student.Add(key, course);
